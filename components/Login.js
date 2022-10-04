4594964c3613a2583a8a6deeb8f9bbdd0559c405
Jonesdy99 <jonesdylan1165@gmail.com>
2022-10-04 19:26:19 -0400

    implement base login page

---
+++ components/Login.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Login(){
+  return (
+    <Head>
+      <title>Login</title>
+    </Head>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
+import Login from "../components/Login"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return <Login />
 }
 
 export default MyApp
