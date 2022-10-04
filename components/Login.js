97a9e20a14b9d837c105dd97fb6c0badf6b5d166
Jonesdy99 <jonesdylan1165@gmail.com>
2022-10-04 19:29:48 -0400

    install chakra icons

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
+import { ChatIcon } from "@chakra-ui/icons";
 
 export default function Login(){
   return (
-    <Head>
-      <title>Login</title>
-    </Head>
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <ChatIcon w='100px' h='100px' color='white' />
+    </>
   )
 }
