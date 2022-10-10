edf5621a13da408efe5fbe91248451e4ff2cd328
Jonesdy99 <jonesdylan1165@gmail.com>
2022-10-09 21:40:22 -0400

    minor fix

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { ChatIcon } from "@chakra-ui/icons";
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 export default function Login(){
   return (
@@ -13,9 +13,11 @@ export default function Login(){
         w='fit-content'
         p={5}
         rounded='3x1'
+        boxShadow='md'
       >
         <ChatIcon w='100px' h='100px' color='white' />
       </Box>
+      <Button boxShadow='md'>Sign In with Google</Button>
     </>
   )
 }
