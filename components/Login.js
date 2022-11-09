d215fd3eb4be8e3a298d10d91360d1ec485f91a4
Jonesdy99 <jonesdylan1165@gmail.com>
2022-11-08 21:07:05 -0500

    center sign in page

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { ChatIcon } from "@chakra-ui/icons";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Center, Stack } from "@chakra-ui/react";
 
 export default function Login(){
   return (
@@ -8,17 +8,23 @@ export default function Login(){
       <Head>
         <title>Login</title>
       </Head>
-      <Box
-        bgColor='blue.500'
-        w='fit-content'
-        p={5}
-        rounded='3x1'
-        boxShadow='md'
-        
-      >
-        <ChatIcon w='100px' h='100px' color='white' />
-      </Box>
-      <Button boxShadow='md'>Sign In with Google</Button>
+      <Center
+      h='100vh'>
+        <Stack
+          align='center'
+        >
+          <Box
+            bgColor='blue.500'
+            w='fit-content'
+            p={5}
+            rounded='3x1'
+            boxShadow='md'
+            >
+            <ChatIcon w='100px' h='100px' color='white' />
+          </Box>
+          <Button boxShadow='md'>Sign In with Google</Button>
+        </Stack>
+      </Center>
     </>
   )
 }
